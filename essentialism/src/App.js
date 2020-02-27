60787935182db931997a2e221bc1b5ae510732e4
Josh Hill <josh_hill15@me.com>
2020-02-26 22:29:56 -0500

    added signup and login barebones

---
+++ essentialism/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Login from "./components/Login";
+import Register from "./components/Register";
+import { NavLink, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Route exact path="/" component={Login} />
+      <Route exact path="/" component={Register} />
+    </div>
+  );
+}
+
+export default App;
---
+++ essentialism/src/components/Register.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+//import "./Register.css";
+
+const initialState = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    isFetching: false
+};
+
+const Register = props => {
+    const [data, setData] = useState(initialState);
+
+    const handleChange = event => {
+        setData({ ...data, [event.target.name]: event.target.value });
+    };
+
+    const handleSubmit = event => {
+        event.prevent.default();
+        delete data.isFetching;
+        axios.post("")
+            .then(response => {
+                props.history.push("/");
+            })
+            .catch(error => {
+                console.log("Sorry, an error has occured.", error)
+            });
+    };
+
+    return (
+        <div>
+            <h1>essentialism</h1>
+            <h2>Create a New Account</h2>
+            <form>
+                <label>First Name</label>
+                <input
+                    type="text"
+                    name="firstname"
+                    placeholder="Enter first name"
+                    value={data.firstname}
+                    onChange={handleChange}
+                />
+                <label>Surname</label>
+                <input
+                    type="text"
+                    name="lastname"
+                    placeholder="Enter surname"
+                    value={data.lastname}
+                    onChange={handleChange}
+                />
+                <label>Phone</label>
+                <input
+                    type="text"
+                    name="phone"
+                    placeholder="Enter phone number"
+                    value={data.phone}
+                    onChange={handleChange}
+                />
+                <label>Email</label>
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Enter email address"
+                    value={data.email}
+                    onChange={handleChange}
+                />
+                <label>Password</label>
+                <input
+                    type="text"
+                    name="password"
+                    placeholder="Enter password"
+                    value={data.password}
+                    onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+};
+
+export default Register;
