e918ed66c62da906fd647901c1f7c314ff84eeb6
Josh Hill <josh_hill15@me.com>
2020-03-06 00:05:13 -0500

    built out Values component

--- essentialism/src/components/Values.js
+++ essentialism/src/components/Values.js
@@ -16,10 +16,8 @@ function Values() {
       dispatch({ type: GET_VALUES_START })
       try {
         const { data } = await axios.get("https://essentialism-backend1.herokuapp.com/values")
-        console.log(data)
         setValues(data.values)
         dispatch({ type: GET_VALUES_SUCCESS })
-
       }
       catch (err) {
         console.log(err)
@@ -28,16 +26,46 @@ function Values() {
 
     }
     getValues()
-  }, [])
+  }, [dispatch])
+
+  const token = localStorage.getItem("token")
+  const user_id = localStorage.getItem("user_id")
+
+  async function threeFavoriteValues(e, id) {
+    e.preventDefault()
+    try {
+      dispatch({ type: POST_VALUES_START })
+      await axios({
+        method: "post",
+        url: "https://essentialism-backend1.herokuapp.com/users/uservalues",
+        data: {
+          value_id: id,
+          user_id
+        },
+        headers: {
+          Authorization: token
+        }
+      })
+      dispatch({ type: POST_VALUES_SUCCESS })
+
+    }
+    catch (err) {
+      console.log("err", err)
+      dispatch({ type: POST_VALUES_FAILURE })
+
+    }
+
+  }
 
   return (
     <div>
       <h1>Essentialism</h1>
       <h3>Choose Your Values</h3>
+      {/* {count === 3 && <h3>You've picked all of your values!</h3>} */}
       {error && <p>{error}</p>}
       {isLoading && <Spinner />}
       <OnboardingButton onClick={() => History.push("/pick-three")}>I'm done selecting!</OnboardingButton>
-      {values && values.map(cv => <p key={cv.id}>{cv.value}</p>)}
+      {values && values.map(cv => <p onClick={e => threeFavoriteValues(e, cv.id)} key={cv.id}>{cv.value}</p>)}
     </div>
   )
 }
@@ -47,3 +75,7 @@ export default Values
 export const GET_VALUES_START = 'GET_VALUES_START';
 export const GET_VALUES_SUCCESS = 'GET_VALUES_SUCCESS';
 export const GET_VALUES_FAILURE = 'GET_VALUES_FAILURE';
+
+export const POST_VALUES_START = 'POST_VALUES_START';
+export const POST_VALUES_SUCCESS = 'POST_VALUES_SUCCESS';
+export const POST_VALUES_FAILURE = 'POST_VALUES_FAILURE';
